b3efd8e1943efdde194c63c5aea91c84592d4f9e
guoxiaoying <18233217303@163.com>
2022-01-02 19:37:20 +0800

    消费者服务

---
+++ cloud-consumer-order80/src/main/java/com/gxy/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.gxy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-02 19:06
+ **/
+
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main( String[] args ){
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
