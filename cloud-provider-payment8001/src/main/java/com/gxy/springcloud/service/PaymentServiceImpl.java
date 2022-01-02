d40e791ab6b77f30028a6ec643cb8edbd7e54664
guoxiaoying <18233217303@163.com>
2022-01-02 15:43:02 +0800

    payment

---
+++ cloud-provider-payment8001/src/main/java/com/gxy/springcloud/service/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.gxy.springcloud.service;
+
+
+import com.gxy.springcloud.dao.PaymentDao;
+import com.gxy.springcloud.entity.Payment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
+
