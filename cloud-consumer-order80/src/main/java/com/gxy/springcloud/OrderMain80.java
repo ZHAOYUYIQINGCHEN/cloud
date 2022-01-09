5a0cb676ba56ade9614e824231e0f517feb3a261
guoxiaoying <18233217303@163.com>
2022-01-09 14:56:01 +0800

    consul+ribbon

--- cloud-consumer-order80/src/main/java/com/gxy/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/gxy/springcloud/OrderMain80.java
@@ -1,8 +1,10 @@
 package com.gxy.springcloud;
 
+import com.gxy.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @program: cloud2021
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient//<--- 添加该标签
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main( String[] args ){
         SpringApplication.run(OrderMain80.class, args);
