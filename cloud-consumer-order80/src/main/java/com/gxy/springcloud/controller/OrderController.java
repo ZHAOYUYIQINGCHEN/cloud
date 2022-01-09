e0fa58bcb24121ad1674c446dddc260bc929fb6c
guoxiaoying <18233217303@163.com>
2022-01-09 15:14:11 +0800

    lb

--- cloud-consumer-order80/src/main/java/com/gxy/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/gxy/springcloud/controller/OrderController.java
@@ -2,7 +2,10 @@ package com.gxy.springcloud.controller;
 
 import com.gxy.springcloud.CommonResult;
 import com.gxy.springcloud.Payment;
+import com.gxy.springcloud.rule.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @program: cloud2021
@@ -26,6 +31,13 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
@@ -47,5 +59,21 @@ public class OrderController {
         }else{
             return new CommonResult<>(444,"操作失败");
         }
+    }
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB()
+    {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(instances == null || instances.size() <= 0){
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+
     }
 }
