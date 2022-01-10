2ddb82301e82689b0b89c8a4dd5d989a97b09726
guoxiaoying <18233217303@163.com>
2022-01-11 00:09:03 +0800

    Hystrix之服务降级订单侧fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/gxy/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/gxy/springcloud/service/PaymentService.java
@@ -23,13 +23,13 @@ public class PaymentService {
     }
 
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler"/*指定善后方法名*/,commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="8000")
     })
     public String paymentInfo_TimeOut(Integer id)
     {
 //        int age = 10/0;
 
-        try { TimeUnit.MILLISECONDS.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
+        try { TimeUnit.MILLISECONDS.sleep(8000); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): 3";
     }
 
