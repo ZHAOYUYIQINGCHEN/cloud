eabb3029d506a0743437167fbc954b21106e68f3
guoxiaoying <18233217303@163.com>
2022-01-10 08:30:48 +0800

    hystrix

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/gxy/springcloud/service/PaymentService.java
@@ -0,0 +1,29 @@
+package com.gxy.springcloud.service;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-10 08:28
+ **/
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Service
+public class PaymentService {
+    /**
+     */
+    public String paymentInfo_OK(Integer id)
+    {
+        return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
+    }
+
+    public String paymentInfo_TimeOut(Integer id)
+    {
+        try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
+        return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): 3";
+    }
+}
+
