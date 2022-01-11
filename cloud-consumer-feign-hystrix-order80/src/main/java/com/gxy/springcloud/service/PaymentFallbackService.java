ce9e53f0353ff1cdcd233fdccfb5f4045406f050
guoxiaoying <18233217303@163.com>
2022-01-11 08:56:02 +0800

    Hystrix之通配服务降级FeignFallback

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/gxy/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,27 @@
+package com.gxy.springcloud.service;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-11 08:54
+ **/
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService
+{
+    @Override
+    public String paymentInfo_OK(Integer id)
+    {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id)
+    {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
+
