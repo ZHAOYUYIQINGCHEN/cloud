3f2950c4d630d19909e6b01ce34babd4ac7b0f92
guoxiaoying <18233217303@163.com>
2022-01-25 22:34:51 +0800

    Config配置总控中心搭建

---
+++ cloud-config-center-3344/src/main/java/com/gxy/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.gxy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-25 22:26
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
