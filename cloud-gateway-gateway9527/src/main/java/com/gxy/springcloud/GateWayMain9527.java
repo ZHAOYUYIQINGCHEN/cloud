29366050af647bf11374e043d2b165980e7b70a5
guoxiaoying <18233217303@163.com>
2022-01-25 22:23:50 +0800

    GateWay

---
+++ cloud-gateway-gateway9527/src/main/java/com/gxy/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.gxy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-24 21:59
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527
+{
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/gxy/springcloud/config/GateWayConfig.java
@@ -0,0 +1,27 @@
+package com.gxy.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: cloud2021
+ * @description:
+ * @author: guoxiaoying01
+ * @create: 2022-01-24 22:18
+ **/
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+}
